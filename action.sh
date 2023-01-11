79449d613d37e03f888b86843f7438fd71453d28
nahwtflomdaasdfgbhnm <122455809+nahwtflomdaasdfgbhnm@users.noreply.github.com>
2023-01-11 23:01:55 +0530

    Delete action.sh

--- action.sh
+++
@@ -1,6 +0,0 @@
-branch=main
-git clone -b $branch https://github.com/New-Dev3/Ultroid /root/New-Dev3
-cp ultroid/.env /root/New-Dev3/.env
-cd /root/New-Dev3
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
